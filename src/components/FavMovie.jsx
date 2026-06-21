72189a58f00df73569093759c6f72e3d55888065
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-06-21 22:09:18 +0300

    Add FavMovie component to display favorite movies

---
+++ src/components/FavMovie.jsx
@@ -0,0 +1,16 @@
+import {useDispatch} from 'react-redux';
+import {removeMovieToFavs} from '../store/actions';
+export default function FavMovie({ title, id }) {
+  const dispatch=useDispatch();
+  const removeHandler=()=>{
+    dispatch(removeMovieToFavs(id));
+  }
+  return (
+    <div className="flex p-3 pl-4 bg-white mb-2 shadow items-center group">
+      <div className="pr-4 flex-1">{title}</div>
+      <button onClick={removeHandler} className="px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100">
+        Çıkar
+      </button>
+    </div>
+  );
+}
