a3db66440acf17aa9c933476f3e5cc858e39ecf3
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-06-21 22:12:21 +0300

    Initialize Redux store with logger middleware

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import {legacy_createStore as createStore,applyMiddleware } from 'redux';
+import reducer from './reducers';
+import { logger } from 'redux-logger';
+export const myStore = createStore(
+  reducer,
+  applyMiddleware(logger)
+);
